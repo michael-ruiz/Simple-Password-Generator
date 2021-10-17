c4c950a4006eb116512b0dd6148fb21c016d4513
Michael Ruiz <mruiz6@uwo.ca>
2021-10-17 18:53:01 -0400

    added basic encrypt and decrypt functions

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function passGen(len, specialChars)
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let charactersAndSpecial = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*'
 
-    if (specialChars != 0) 
+    if (specialChars) 
     {
         for (var i = 0; i < len; i++) 
         {
@@ -20,9 +20,43 @@ function passGen(len, specialChars)
         }
     }
 
-    console.log(`Here is your password: ${password}`);
+    return password;
 }
 
-len = 15;
-passGen(len, 0);
-passGen(len, 1);
+function basicEncrypt(password, key)
+{
+   let encryptedPassword = '';
+   for (let i = 0; i < password.length; i++) 
+   {
+       let asciiNum = password.charCodeAt(i) + key;
+       encryptedPassword += String.fromCharCode(asciiNum);
+   }
+   return encryptedPassword;
+}
+
+function basicDecrypt(password, key)
+{
+    let decryptedPassword = '';
+    for (let i = 0; i < password.length; i++) 
+    {
+        let asciiNum = password.charCodeAt(i) - key;
+        decryptedPassword += String.fromCharCode(asciiNum);
+    }
+    return decryptedPassword;
+}
+
+const LEN = 8;
+
+password1 = passGen(LEN, false);
+encryptedPassword1 = basicEncrypt(password1, 5);
+password2 = passGen(LEN, true);
+encryptedPassword2 = basicEncrypt(password2, 5);
+
+console.log(`Here is your password without special characters: ${password1}`);
+console.log(`Here is your password with special characters: ${password2}`);
+
+console.log(`Here is your encrypted password without special characters: ${encryptedPassword1}`);
+console.log(`Here is your encrypted password with special characters: ${encryptedPassword2}`);
+
+console.log(`Here is the decrypted password without special characters: ${basicDecrypt(encryptedPassword1, 5)}`);
+console.log(`Here is the decrypted password with special characters: ${basicDecrypt(encryptedPassword2, 5)}`);
